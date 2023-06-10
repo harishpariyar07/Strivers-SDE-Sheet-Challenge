2002f1e03a204c1918f3b3c3cfa00e8804c87449
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-10 14:38:37 +0545

    question 26 done

---
+++ Linkedlist-I/find_middle_of_linkedlist.cpp
@@ -0,0 +1,13 @@
+// Slow and fast pointer concept
+
+Node *findMiddle(Node *head)
+{
+    // Write your code here
+    Node *slow = head, *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
