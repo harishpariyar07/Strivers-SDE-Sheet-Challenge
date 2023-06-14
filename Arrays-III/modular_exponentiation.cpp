c4e90bb4a13ab31a4038fdb0e828cd4ccea9a4cd
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-14 18:09:41 +0545

    question 14 done

---
+++ Arrays-III/modular_exponentiation.cpp
@@ -0,0 +1,34 @@
+// the concept is quite easy
+// it's maths but the way to code it is quite troublesome
+
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m)
+{
+
+    if (m == 1)
+    {
+
+        return 0; // Invalid case, as all numbers are congruent to 0 modulo 1
+    }
+
+    int res = 1 % m;
+
+    x = x % m;
+
+    while (n > 0)
+    {
+
+        if (n & 1)
+        {
+
+            res = static_cast<long long>(res) * x % m;
+        }
+
+        x = static_cast<long long>(x) * x % m;
+
+        n >>= 1;
+    }
+
+    return res;
+}
